496057ba1252c89fdbad9e60549b63de45eb41d9
jelopf <97445070+jelopf@users.noreply.github.com>
2024-05-08 18:23:14 +0500

    Перестроили структуру шаблона в MVC

---
+++ gameproject/RhythmGame.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace gameproject
+{
+    public class GameModel
+    {
+        // Логика и данные игры
+    }
+
+    public class GameView
+    {
+        private SpriteBatch _spriteBatch;
+        private GameModel _model;
+
+        public GameView(SpriteBatch spriteBatch, GameModel model)
+        {
+            _spriteBatch = spriteBatch;
+            _model = model;
+        }
+
+        public void Draw()
+        {
+            // Рисование игры, используя _spriteBatch и _model
+        }
+    }
+
+    public class GameController
+    {
+        private GameModel _model;
+
+        public GameController(GameModel model)
+        {
+            _model = model;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // Обновление модели, основанное на пользовательском вводе
+        }
+    }
+
+    // Основной класс игры
+    public class RhythmGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private GameModel _model;
+        private GameView _view;
+        private GameController _controller;
+
+        public RhythmGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _model = new GameModel();
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _view = new GameView(_spriteBatch, _model);
+            _controller = new GameController(_model);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Загрузка контента
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            _controller.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _view.Draw();
+
+            base.Draw(gameTime);
+        }
+    }
+}
